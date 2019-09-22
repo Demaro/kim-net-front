b5a0d6caa34d0ccc00ea32ff211bbbef3161223d
demaro <demaromail@gmail.com>
2019-09-21 22:01:18 -0300

    not api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:4000',
   firebase: {
-    apiKey: "AIzaSyCEBym69Fwd8A0wW7SL8NG8-ThI0_Z_v3g",
+    apiKey: "nopasana!",
     authDomain: "kim-gas.firebaseapp.com",
-    databaseURL: "https://kim-gas.firebaseio.com",
+    databaseURL: "https://kim-gas.firebaseio. com",
     projectId: "kim-gas",
     storageBucket: "",
     messagingSenderId: "141222314397",
