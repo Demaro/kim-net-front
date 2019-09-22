042b739d525d8066cbbc23c11b8e1d5c703a9109
demaro <demaromail@gmail.com>
2019-09-21 21:48:56 -0300

    fix enviroment in prod firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
+  apiUrl: 'http://localhost:4000',
+  firebase: {
+    apiKey: "AIzaSyCEBym69Fwd8A0wW7SL8NG8-ThI0_Z_v3g",
+    authDomain: "kim-gas.firebaseapp.com",
+    databaseURL: "https://kim-gas.firebaseio.com",
+    projectId: "kim-gas",
+    storageBucket: "",
+    messagingSenderId: "141222314397",
+    appId: "1:141222314397:web:c85d144f0637c03dc56f74"
+  }
   
 };
