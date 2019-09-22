a71202be6998a9587d9f07b6c69c86544cdc7d93
demaro <demaromail@gmail.com>
2019-09-21 23:16:12 -0300

    go deploy ready

--- src/app/login/login.routes.ts
+++ src/app/login/login.routes.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
 import { LoginComponent } from './login.component';
-import { AuthGuard } from '../helpers/auth-guard.service';
+
 export const LoginRoutes: Route[] = [
   {
     path: 'login',
